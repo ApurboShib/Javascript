f5529601d38ef63eac50eb6b11f6f9e947ad2520
ApurboShib <shibjoy088@gmail.com>
2025-01-08 23:10:07 +0600

    updated

--- array.js
+++ array.js
@@ -27,3 +27,18 @@ console.log("the lenthth of array is :" , hello[4]);
 let abc = 38743;
 abc = 10;
 console.log(abc);
+
+// insert value on array
+
+h.push("joy");
+console.log(h);
+h.pop();
+console.log(h);
+
+// first value remove korrar jonno shift() use korbo
+console.log(hello);
+hello.shift();
+console.log(hello);
+hello.unshift(100);
+console.log(hello);
+
