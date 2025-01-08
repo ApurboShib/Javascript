ea6adfa1019e4d0d333b8d2e9a87d4c2b824f0cf
ApurboShib <shibjoy088@gmail.com>
2025-01-08 22:43:07 +0600

    add if-else

---
+++ conditionals.js
@@ -0,0 +1,21 @@
+console.log(238 > 38);
+console.log(238 < 38);
+console.log(238 >= 38);
+console.log(238 <= 38);
+console.log(238 > 38);
+console.log(238 == 38);
+console.log(238 != 38);
+console.log(238 > 38);
+console.log(238 <= 38);
+// multi-condional statements...
+
+
+if(100 < 19) {
+    console.log("this is true");
+}
+else if (100 == 100) {
+    console.log("this is hardly yes");
+}
+else{
+    console.log("this is false");
+}
