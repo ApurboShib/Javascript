fe16f163719effb8a7a024aae757cb7c0449b2cc
ApurboShib <shibjoy088@gmail.com>
2025-01-08 21:15:56 +0600

    variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+var x = 100;
+var y = "hello";
+var name = "Joy shib"
+var money;
+var isRich = false
+money = 949374928308;
+var mission = false;
+console.log(x);
+console.log(y);
+console.log(money);
+console.log(name)
+console.log(isRich);
+console.log(typeof(mission));
+
