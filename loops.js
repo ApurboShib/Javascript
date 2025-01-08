9daa9628ffe121e58fc73a6c37b8f40eb385d67e
ApurboShib <shibjoy088@gmail.com>
2025-01-08 23:37:09 +0600

    add while loops

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 const hello = [2,3,5,44,2,3,5,2,4,2,4,2,4,22];
 
-// traversed the loops..
+// traversed the for loops..
 for(const x of hello) {
     console.log(x);
 }
@@ -10,4 +10,12 @@ const fruites = ["mango", "orange", "jdskajd"];
 for(const x of fruites) {
     console.log(x);
     console.log("hello from gandu");
+}
+
+//  traversed the while loops...
+
+let num = 0;
+while(num <= 10) {
+    console.log( " hdjahdkjdkjakdja", num);
+    num++;
 }
