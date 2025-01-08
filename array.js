b78895ecbfe53d179eb2a2d5370433bd21e4816e
ApurboShib <shibjoy088@gmail.com>
2025-01-08 23:04:05 +0600

    added array

---
+++ array.js
@@ -0,0 +1,29 @@
+// array diclerations..
+
+// intteger array
+const hello = [1,2,3,4,5,6,8,10];
+// string array
+
+const h = ["abul", "babul", "kabul", "habul", "bulbul"];
+
+// mixed array delc
+
+const k = [2,3,5,'2', '3', 'r', "hello"];
+
+// length of the arry is.
+
+console.log(hello.length);
+
+// printing the array
+
+console.log(hello);
+
+//print the array indext value.
+
+console.log("the lenthth of array is :" , hello[4]);
+
+// set or update the value
+
+let abc = 38743;
+abc = 10;
+console.log(abc);
