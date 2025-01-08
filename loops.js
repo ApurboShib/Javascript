3db1a51e6dae31a799c7b2f2ca28e99723fc648e
ApurboShib <shibjoy088@gmail.com>
2025-01-08 23:33:00 +0600

    loops added

---
+++ loops.js
@@ -0,0 +1,13 @@
+const hello = [2,3,5,44,2,3,5,2,4,2,4,2,4,22];
+
+// traversed the loops..
+for(const x of hello) {
+    console.log(x);
+}
+
+const fruites = ["mango", "orange", "jdskajd"];
+
+for(const x of fruites) {
+    console.log(x);
+    console.log("hello from gandu");
+}
