2e62c27c5daba3432d4dd42a987b94c7b398fba6
ApurboShib <shibjoy088@gmail.com>
2025-01-08 22:14:28 +0600

    add number

---
+++ number.js
@@ -0,0 +1,11 @@
+var x = 29283;
+var y = 3878;
+var z = 83.37287;
+var a = '84';
+var b = parseInt(a);
+console.log(x + y);
+console.log(typeof(x + a));
+console.log(typeof(b));
+
+// here we use setpression as a toFixed()
+console.log(z.toFixed(19));
