6be57967e21b94a644167cf7d1601e35c3fc5abd
ApurboShib <shibjoy088@gmail.com>
2025-01-08 22:18:17 +0600

    add operation

---
+++ operations.js
@@ -0,0 +1,10 @@
+var a = 3747399;
+var b = 83781731;
+var c = 794719;
+
+console.log(a + b + c);
+console.log(a - b - c);
+console.log(a * b * c);
+console.log(a / b / c);
+console.log(a + b % c);
+console.log(100 % 5);
