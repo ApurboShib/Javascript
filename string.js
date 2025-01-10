68bcf421b9e16606f488a5c2fc61b4034fc1782c
ApurboShib <shibjoy088@gmail.com>
2025-01-10 21:37:26 +0600

    string added

---
+++ string.js
@@ -0,0 +1,25 @@
+const name = "joy";
+const position = "super"
+const ans = new String("power"); // this is an object
+
+console.log(typeof(name));
+console.log(typeof(position));
+console.log(typeof(ans));
+
+// normal length() use kore amra string er character cnt  ber korte parbo
+
+const test = "hdsjfgshfghsdfgmhsgfhgf";
+console.log(test.length);
+
+const check = "hshfsgdfyusdgnsdbvfwgfsb";
+const res = check.slice(2, 10);
+console.log(res);
+
+// const word = "ami ekta santo sristo leg bisisto chele";
+const word = ['ajke', 'amar', 'Mon', 'valo', 'nai'];
+// console.log(word);
+// console.log(word.split(''));
+// console.log(word.split(' '));
+console.log(word.join());
+console.log(word.join('.'));
+console.log(word.join('|'));
