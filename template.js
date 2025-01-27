d3d93acf15305b83a6a3d5b8948dc9c46e17bb90
ApurboShib <shibjoy088@gmail.com>
2025-01-27 22:31:49 +0600

    add bactik

---
+++ template.js
@@ -0,0 +1,19 @@
+const a = 100;
+const b = 200;
+const ans = 'the sum of ' + a + 'and' + b + 'is' + (a + b);
+console.log(ans);
+
+// use bactik (` `).
+const result = `the sum of ${a} and ${b} is ${a + b}`
+console.log(result);
+const email = 'jhon shina \n' + 
+'cena cena lage \n'
+'tmi maramari korba naki'
+
+console.log(email);
+const challenge = `john shina
+tmi je hou na kno
+tilaghor aso
+khela hobe`
+
+console.log(challenge);
